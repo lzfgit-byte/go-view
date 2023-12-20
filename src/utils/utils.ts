81c87d923503d0bc34db005e92b99d14a9a0c526
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-20 10:52:00 +0800

    fix:reformat

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,32 +1,33 @@
-import { h } from 'vue'
-import { NIcon } from 'naive-ui'
-import screenfull from 'screenfull'
-import throttle from 'lodash/throttle'
-import Image_404 from '../assets/images/exception/image-404.png'
-import html2canvas from 'html2canvas'
-import { downloadByA } from './file'
-import { toString } from './type'
-import cloneDeep from 'lodash/cloneDeep'
-import { WinKeyboard } from '@/enums/editPageEnum'
-import { RequestHttpIntervalEnum, RequestParamsObjType } from '@/enums/httpEnum'
-import { CreateComponentType, CreateComponentGroupType } from '@/packages/index.d'
-import { excludeParseEventKeyList, excludeParseEventValueList } from '@/enums/eventEnum'
+import { h } from 'vue';
+import { NIcon } from 'naive-ui';
+import screenfull from 'screenfull';
+import throttle from 'lodash/throttle';
+import html2canvas from 'html2canvas';
+import cloneDeep from 'lodash/cloneDeep';
+import Image_404 from '../assets/images/exception/image-404.png';
+import { downloadByA } from './file';
+import { toString } from './type';
+import { WinKeyboard } from '@/enums/editPageEnum';
+import type { RequestParamsObjType } from '@/enums/httpEnum';
+import { RequestHttpIntervalEnum } from '@/enums/httpEnum';
+import type { CreateComponentGroupType, CreateComponentType } from '@/packages/index.d';
+import { excludeParseEventKeyList, excludeParseEventValueList } from '@/enums/eventEnum';
 
 /**
  * * 判断是否是开发环境
  * @return { Boolean }
  */
 export const isDev = () => {
-  return import.meta.env.DEV
-}
+  return import.meta.env.DEV;
+};
 
 /**
  * * 生成一个不重复的ID
  * @param { Number } randomLength
  */
 export const getUUID = (randomLength = 10) => {
-  return Number(Math.random().toString().substring(2, randomLength) + Date.now()).toString(36)
-}
+  return Number(Math.random().toString().substring(2, randomLength) + Date.now()).toString(36);
+};
 
 /**
  * * render 图标
@@ -34,8 +35,8 @@ export const getUUID = (randomLength = 10) => {
  *  @param set 设置项
  */
 export const renderIcon = (icon: any, set = {}) => {
-  return () => h(NIcon, set, { default: () => h(icon) })
-}
+  return () => h(NIcon, set, { default: () => h(icon) });
+};
 /**
  * * render 语言
  *  @param lang 语言标识
@@ -43,16 +44,16 @@ export const renderIcon = (icon: any, set = {}) => {
  *  @param tag 要渲染成的标签
  */
 export const renderLang = (lang: string, set = {}, tag = 'span') => {
-  return () => h(tag, set, { default: () => window['$t'](lang) })
-}
+  return () => h(tag, set, { default: () => window['$t'](lang) });
+};
 
 /**
  * * 获取错误处理图片，默认 404 图
  * @returns url
  */
 export const requireErrorImg = () => {
-  return Image_404
-}
+  return Image_404;
+};
 
 /**
  * * 全屏操作函数
@@ -62,18 +63,18 @@ export const requireErrorImg = () => {
  */
 export const screenfullFn = (isFullscreen?: boolean, isEnabled?: boolean) => {
   // 是否是全屏
-  if (isFullscreen) return screenfull.isFullscreen
+  if (isFullscreen) return screenfull.isFullscreen;
 
   // 是否支持全屏
-  if (isEnabled) return screenfull.isEnabled
+  if (isEnabled) return screenfull.isEnabled;
 
   if (screenfull.isEnabled) {
-    screenfull.toggle()
-    return
+    screenfull.toggle();
+    return;
   }
   // TODO lang
-  window['$message'].warning('您的浏览器不支持全屏功能！')
-}
+  window['$message'].warning('您的浏览器不支持全屏功能！');
+};
 
 /**
  * 修改元素位置
@@ -86,9 +87,9 @@ export const setComponentPosition = (
   x?: number,
   y?: number
 ) => {
-  x && (target.attr.x = x)
-  y && (target.attr.y = y)
-}
+  x && (target.attr.x = x);
+  y && (target.attr.y = y);
+};
 
 /**
  * * 设置元素属性
@@ -96,59 +97,62 @@ export const setComponentPosition = (
  * @param key 键名
  * @param value 键值
  */
-export const setDomAttribute = <K extends keyof CSSStyleDeclaration, V extends CSSStyleDeclaration[K]>(
+export const setDomAttribute = <
+  K extends keyof CSSStyleDeclaration,
+  V extends CSSStyleDeclaration[K]
+>(
   HTMLElement: HTMLElement,
   key: K,
   value: V
 ) => {
   if (HTMLElement) {
-    HTMLElement.style[key] = value
+    HTMLElement.style[key] = value;
   }
-}
+};
 
 /**
  * * 判断是否是 mac
  * @returns boolean
  */
 export const isMac = () => {
-  return /macintosh|mac os x/i.test(navigator.userAgent)
-}
+  return /macintosh|mac os x/i.test(navigator.userAgent);
+};
 
 /**
  * * 挂载监听
  */
-// eslint-disable-next-line no-undef
+
 export const addEventListener = <K extends keyof WindowEventMap>(
   target: HTMLElement | Document,
   type: K,
   listener: any,
   delay?: number,
-  // eslint-disable-next-line no-undef
+
   options?: boolean | AddEventListenerOptions | undefined
 ) => {
-  if (!target) return
+  if (!target) return;
   target.addEventListener(
     type,
     throttle(listener, delay || 300, {
       leading: true,
-      trailing: false
+      trailing: false,
     }),
     options
-  )
-}
+  );
+};
 
 /**
  * * 卸载监听
  */
-// eslint-disable-next-line no-undef
+
 export const removeEventListener = <K extends keyof WindowEventMap>(
   target: HTMLElement | Document,
   type: K,
   listener: any
 ) => {
-  if (!target) return
-  target.removeEventListener(type, listener)
-}
+  if (!target) return;
+  target.removeEventListener(type, listener);
+};
 
 /**
  * * 截取画面为图片并下载
@@ -156,21 +160,21 @@ export const removeEventListener = <K extends keyof WindowEventMap>(
  */
 export const canvasCut = (html: HTMLElement | null, callback?: Function) => {
   if (!html) {
-    window['$message'].error('导出失败！')
-    if (callback) callback()
-    return
+    window['$message'].error('导出失败！');
+    if (callback) callback();
+    return;
   }
 
   html2canvas(html, {
     backgroundColor: null,
     allowTaint: true,
-    useCORS: true
+    useCORS: true,
   }).then((canvas: HTMLCanvasElement) => {
-    window['$message'].success('导出成功！')
-    downloadByA(canvas.toDataURL(), undefined, 'png')
-    if (callback) callback()
-  })
-}
+    window['$message'].success('导出成功！');
+    downloadByA(canvas.toDataURL(), undefined, 'png');
+    if (callback) callback();
+  });
+};
 
 /**
  * * 函数过滤器
@@ -191,19 +195,19 @@ export const newFunctionHandle = (
   successCallBack?: Function
 ) => {
   try {
-    if (!funcStr) return data
-    const fn = new Function('data', 'res', funcStr)
-    const fnRes = fn(cloneDeep(data), cloneDeep(res))
-    const resHandle = isToString ? toString(fnRes) : fnRes
+    if (!funcStr) return data;
+    const fn = new Function('data', 'res', funcStr);
+    const fnRes = fn(cloneDeep(data), cloneDeep(res));
+    const resHandle = isToString ? toString(fnRes) : fnRes;
     // 成功回调
-    successCallBack && successCallBack(resHandle)
-    return resHandle
+    successCallBack && successCallBack(resHandle);
+    return resHandle;
   } catch (error) {
     // 失败回调
-    errorCallBack && errorCallBack(error)
-    return '函数执行错误'
+    errorCallBack && errorCallBack(error);
+    return '函数执行错误';
   }
-}
+};
 
 /**
  * * 处理请求事件单位
@@ -215,20 +219,20 @@ export const intervalUnitHandle = (num: number, unit: RequestHttpIntervalEnum) =
   switch (unit) {
     // 秒
     case RequestHttpIntervalEnum.SECOND:
-      return num * 1000
+      return num * 1000;
     // 分
     case RequestHttpIntervalEnum.MINUTE:
-      return num * 1000 * 60
+      return num * 1000 * 60;
     // 时
     case RequestHttpIntervalEnum.HOUR:
-      return num * 1000 * 60 * 60
+      return num * 1000 * 60 * 60;
     // 天
     case RequestHttpIntervalEnum.DAY:
-      return num * 1000 * 60 * 60 * 24
+      return num * 1000 * 60 * 60 * 24;
     default:
-      return num * 1000
+      return num * 1000;
   }
-}
+};
 
 /**
  * * 对象转换 cookie 格式
@@ -236,14 +240,14 @@ export const intervalUnitHandle = (num: number, unit: RequestHttpIntervalEnum) =
  * @returns string
  */
 export const objToCookie = (obj: RequestParamsObjType) => {
-  if (!obj) return ''
+  if (!obj) return '';
 
-  let str = ''
+  let str = '';
   for (const key in obj) {
-    str += key + '=' + obj[key] + ';'
+    str += `${key}=${obj[key]};`;
   }
-  return str.substring(0, str.length - 1)
-}
+  return str.substring(0, str.length - 1);
+};
 
 /**
  * * 设置按下键盘按键的底部展示
@@ -253,21 +257,25 @@ export const objToCookie = (obj: RequestParamsObjType) => {
 export const setKeyboardDressShow = (keyCode?: number) => {
   const code = new Map([
     [17, WinKeyboard.CTRL],
-    [32, WinKeyboard.SPACE]
-  ])
+    [32, WinKeyboard.SPACE],
+  ]);
 
-  const dom = document.getElementById('keyboard-dress-show')
-  if (!dom) return
+  const dom = document.getElementById('keyboard-dress-show');
+  if (!dom) {
+    return;
+  }
   if (!keyCode) {
-    window.onKeySpacePressHold?.(false)
-    dom.innerText = ''
-    return
+    window.onKeySpacePressHold?.(false);
+    dom.innerText = '';
+    return;
   }
   if (keyCode && code.has(keyCode)) {
-    if (keyCode == 32) window.onKeySpacePressHold?.(true)
-    dom.innerText = `按下了「${code.get(keyCode)}」键`
+    if (keyCode === 32) {
+      window.onKeySpacePressHold?.(true);
+    }
+    dom.innerText = `按下了「${code.get(keyCode)}」键`;
   }
-}
+};
 
 /**
  * * JSON序列化，支持函数和 undefined
@@ -279,22 +287,22 @@ export const JSONStringify = <T>(data: T) => {
     (key, val) => {
       // 处理函数丢失问题
       if (typeof val === 'function') {
-        return `${val}`
+        return `${val}`;
       }
       // 处理 undefined 丢失问题
       if (typeof val === 'undefined') {
-        return null
+        return null;
       }
-      return val
+      return val;
     },
     2
-  )
-}
+  );
+};
 
 export const evalFn = (fn: string) => {
-  var Fun = Function // 一个变量指向Function，防止前端编译工具报错
-  return new Fun('return ' + fn)()
-}
+  let Fun = Function; // 一个变量指向Function，防止前端编译工具报错
+  return new Fun(`return ${fn}`)();
+};
 
 /**
  * * JSON反序列化，支持函数和 undefined
@@ -303,33 +311,43 @@ export const evalFn = (fn: string) => {
 export const JSONParse = (data: string) => {
   return JSON.parse(data, (k, v) => {
     // 过滤函数字符串
-    if (excludeParseEventKeyList.includes(k)) return v
+    if (excludeParseEventKeyList.includes(k)) {
+      return v;
+    }
     // 过滤函数值表达式
     if (typeof v === 'string') {
-      const someValue = excludeParseEventValueList.some(excludeValue => v.indexOf(excludeValue) > -1)
-      if (someValue) return v
-    }
-    // 还原函数值
-    if (typeof v === 'string' && v.indexOf && (v.indexOf('function') > -1 || v.indexOf('=>') > -1)) {
-      return evalFn(`(function(){return ${v}})()`)
-    } else if (typeof v === 'string' && v.indexOf && v.indexOf('return ') > -1) {
-      const baseLeftIndex = v.indexOf('(')
-      if (baseLeftIndex > -1) {
-        const newFn = `function ${v.substring(baseLeftIndex)}`
-        return evalFn(`(function(){return ${newFn}})()`)
+      const someValue = excludeParseEventValueList.some(
+        (excludeValue) => v.indexOf(excludeValue) > -1
+      );
+      if (someValue) {
+        return v;
       }
     }
-    return v
-  })
-}
+    // 还原函数值
+    if (
+      typeof v === 'string' &&
+      v.indexOf &&
+      (v.indexOf('function') > -1 || v.indexOf('=>') > -1)
+    ) {
+      return evalFn(`(function(){return ${v}})()`);
+    } else if (typeof v === 'string' && v.indexOf && v.indexOf('return ') > -1) {
+      const baseLeftIndex = v.indexOf('(');
+      if (baseLeftIndex > -1) {
+        const newFn = `function ${v.substring(baseLeftIndex)}`;
+        return evalFn(`(function(){return ${newFn}})()`);
+      }
+    }
+    return v;
+  });
+};
 
 /**
  * * 修改顶部标题
  * @param title
  */
 export const setTitle = (title?: string) => {
-  title && (document.title = title)
-}
+  title && (document.title = title);
+};
 
 /**
  * 处理网页关闭事件
@@ -337,16 +355,16 @@ export const setTitle = (title?: string) => {
 export const addWindowUnload = () => {
   // 关闭网页出现离开提示
   window.onbeforeunload = function (e) {
-    e = e || window.event
+    e = e || window.event;
     // 兼容IE8和Firefox 4之前的版本
     if (e) {
-      e.returnValue = '您确定要离开当前页面吗？请确认是否保存数据！'
+      e.returnValue = '您确定要离开当前页面吗？请确认是否保存数据！';
     }
     // Chrome, Safari, Firefox 4+, Opera 12+ , IE 9+
-    return '您确定要离开当前页面吗？请确认是否保存数据！'
-  }
+    return '您确定要离开当前页面吗？请确认是否保存数据！';
+  };
   // 返回销毁事件函数
   return () => {
-    window.onbeforeunload = null
-  }
-}
+    window.onbeforeunload = null;
+  };
+};
--- src/views/chart/ContentEdit/index.vue
+++ src/views/chart/ContentEdit/index.vue
@@ -1,41 +1,41 @@
 <template>
   <!-- <edit-rule></edit-rule> -->
-  <content-box
+  <ContentBox
     id="go-chart-edit-layout"
     :flex="true"
-    :showTop="false"
-    :showBottom="true"
+    :show-top="false"
+    :show-bottom="true"
     :depth="1"
-    :xScroll="true"
-    :disabledScroll="true"
+    :x-scroll="true"
+    :disabled-scroll="true"
     @mousedown="mousedownHandleUnStop"
     @drop="dragHandle"
     @dragover="dragoverHandle"
     @dragenter="dragoverHandle"
   >
-    <edit-rule>
+    <EditRule>
       <!-- 画布主体 -->
       <div id="go-chart-edit-content" @contextmenu="handleContextMenu">
         <!-- 展示 -->
-        <edit-range>
+        <EditRange>
           <!-- 滤镜预览 -->
           <div
             :style="{
               ...getFilterStyle(chartEditStore.getEditCanvasConfig),
-              ...rangeStyle
+              ...rangeStyle,
             }"
           >
             <!-- 图表 -->
             <div v-for="(item, index) in chartEditStore.getComponentList" :key="item.id">
               <!-- 分组 -->
-              <edit-group
+              <EditGroup
                 v-if="item.isGroup"
-                :groupData="(item as CreateComponentGroupType)"
-                :groupIndex="index"
-              ></edit-group>
+                :group-data="(item as CreateComponentGroupType)"
+                :group-index="index"
+              ></EditGroup>
 
               <!-- 单组件 -->
-              <edit-shape-box
+              <EditShapeBox
                 v-else
                 :data-id="item.id"
                 :index="index"
@@ -51,173 +51,180 @@
                 @contextmenu="handleContextMenu($event, item, optionsHandle)"
               >
                 <component
+                  :is="item.chartConfig.chartKey"
                   class="edit-content-chart"
                   :class="animationsClass(item.styles.animations)"
-                  :is="item.chartConfig.chartKey"
-                  :chartConfig="item"
-                  :themeSetting="themeSetting"
-                  :themeColor="themeColor"
+                  :chart-config="item"
+                  :theme-setting="themeSetting"
+                  :theme-color="themeColor"
                   :style="{
                     ...useSizeStyle(item.attr),
                     ...getFilterStyle(item.styles),
-                    ...getTransformStyle(item.styles)
+                    ...getTransformStyle(item.styles),
                   }"
                 ></component>
-              </edit-shape-box>
+              </EditShapeBox>
             </div>
           </div>
-        </edit-range>
+        </EditRange>
       </div>
-    </edit-rule>
+    </EditRule>
 
     <!-- 工具栏 -->
     <template #aside>
-      <edit-tools></edit-tools>
+      <EditTools></EditTools>
     </template>
 
     <!-- 底部控制 -->
     <template #bottom>
       <EditBottom></EditBottom>
     </template>
-  </content-box>
+  </ContentBox>
 </template>
 
 <script lang="ts" setup>
-import { onMounted, computed, provide } from 'vue'
-import { chartColors } from '@/settings/chartThemes/index'
-import { MenuEnum } from '@/enums/editPageEnum'
-import { CreateComponentType, CreateComponentGroupType } from '@/packages/index.d'
-import {
-  animationsClass,
-  getFilterStyle,
-  getTransformStyle,
-  getBlendModeStyle,
-  colorCustomMerge,
-  addWindowUnload
-} from '@/utils'
-import { useContextMenu } from '@/views/chart/hooks/useContextMenu.hook'
-import { MenuOptionsItemType } from '@/views/chart/hooks/useContextMenu.hook.d'
-import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore'
-import { SCALE_KEY } from '@/views/preview/hooks/useScale.hook'
-import { useLayout } from './hooks/useLayout.hook'
-import { useAddKeyboard } from '../hooks/useKeyboard.hook'
-import { useSync } from '../hooks/useSync.hook'
-import { dragHandle, dragoverHandle, mousedownHandleUnStop, useMouseHandle } from './hooks/useDrag.hook'
-import { useComponentStyle, useSizeStyle } from './hooks/useStyle.hook'
+  import { computed, onMounted, provide } from 'vue';
+  import { useAddKeyboard } from '../hooks/useKeyboard.hook';
+  import { useSync } from '../hooks/useSync.hook';
+  import { ContentBox } from '../ContentBox/index';
+  import { useLayout } from './hooks/useLayout.hook';
+  import {
+    dragHandle,
+    dragoverHandle,
+    mousedownHandleUnStop,
+    useMouseHandle,
+  } from './hooks/useDrag.hook';
+  import { useComponentStyle, useSizeStyle } from './hooks/useStyle.hook';
 
-import { ContentBox } from '../ContentBox/index'
-import { EditGroup } from './components/EditGroup'
-import { EditRange } from './components/EditRange'
-import { EditRule } from './components/EditRule'
-import { EditBottom } from './components/EditBottom'
-import { EditShapeBox } from './components/EditShapeBox'
-import { EditTools } from './components/EditTools'
+  import { EditGroup } from './components/EditGroup';
+  import { EditRange } from './components/EditRange';
+  import { EditRule } from './components/EditRule';
+  import { EditBottom } from './components/EditBottom';
+  import { EditShapeBox } from './components/EditShapeBox';
+  import { EditTools } from './components/EditTools';
+  import { SCALE_KEY } from '@/views/preview/hooks/useScale.hook';
+  import { useChartEditStore } from '@/store/modules/chartEditStore/chartEditStore';
+  import type { MenuOptionsItemType } from '@/views/chart/hooks/useContextMenu.hook.d';
+  import { useContextMenu } from '@/views/chart/hooks/useContextMenu.hook';
+  import {
+    addWindowUnload,
+    animationsClass,
+    colorCustomMerge,
+    getBlendModeStyle,
+    getFilterStyle,
+    getTransformStyle,
+  } from '@/utils';
+  import type { CreateComponentGroupType, CreateComponentType } from '@/packages/index.d';
+  import { MenuEnum } from '@/enums/editPageEnum';
+  import { chartColors } from '@/settings/chartThemes/index';
 
-const chartEditStore = useChartEditStore()
-const { handleContextMenu } = useContextMenu()
-const { dataSyncFetch, intervalDataSyncUpdate } = useSync()
+  const chartEditStore = useChartEditStore();
+  const { handleContextMenu } = useContextMenu();
+  const { dataSyncFetch, intervalDataSyncUpdate } = useSync();
 
-// 加入网页关闭提示 TODO,取消刷新拦截
-addWindowUnload()()
+  // 加入网页关闭提示 TODO,取消刷新拦截
+  addWindowUnload()();
 
-// 编辑时注入scale变量，消除警告
-provide(SCALE_KEY, null)
+  // 编辑时注入scale变量，消除警告
+  provide(SCALE_KEY, null);
 
-// 布局处理
-useLayout(async () => {})
+  // 布局处理
+  useLayout(async () => {});
 
-// 点击事件
-const { mouseenterHandle, mouseleaveHandle, mousedownHandle, mouseClickHandle } = useMouseHandle()
+  // 点击事件
+  const { mouseenterHandle, mouseleaveHandle, mousedownHandle, mouseClickHandle } =
+    useMouseHandle();
 
-// 右键事件
-const optionsHandle = (
-  targetList: MenuOptionsItemType[],
-  allList: MenuOptionsItemType[],
-  targetInstance: CreateComponentType
-) => {
-  // 多选处理
-  if (chartEditStore.getTargetChart.selectId.length > 1) {
-    return allList.filter(i => [MenuEnum.GROUP, MenuEnum.DELETE].includes(i.key as MenuEnum))
-  }
-  const statusMenuEnums: MenuEnum[] = []
-  if (targetInstance.status.lock) {
-    statusMenuEnums.push(MenuEnum.LOCK)
-  } else {
-    statusMenuEnums.push(MenuEnum.UNLOCK)
-  }
-  if (targetInstance.status.hide) {
-    statusMenuEnums.push(MenuEnum.HIDE)
-  } else {
-    statusMenuEnums.push(MenuEnum.SHOW)
-  }
-  return targetList.filter(i => !statusMenuEnums.includes(i.key as MenuEnum))
-}
+  // 右键事件
+  const optionsHandle = (
+    targetList: MenuOptionsItemType[],
+    allList: MenuOptionsItemType[],
+    targetInstance: CreateComponentType
+  ) => {
+    // 多选处理
+    if (chartEditStore.getTargetChart.selectId.length > 1) {
+      return allList.filter((i) => [MenuEnum.GROUP, MenuEnum.DELETE].includes(i.key as MenuEnum));
+    }
+    const statusMenuEnums: MenuEnum[] = [];
+    if (targetInstance.status.lock) {
+      statusMenuEnums.push(MenuEnum.LOCK);
+    } else {
+      statusMenuEnums.push(MenuEnum.UNLOCK);
+    }
+    if (targetInstance.status.hide) {
+      statusMenuEnums.push(MenuEnum.HIDE);
+    } else {
+      statusMenuEnums.push(MenuEnum.SHOW);
+    }
+    return targetList.filter((i) => !statusMenuEnums.includes(i.key as MenuEnum));
+  };
 
-// 主题色
-const themeSetting = computed(() => {
-  const chartThemeSetting = chartEditStore.getEditCanvasConfig.chartThemeSetting
-  return chartThemeSetting
-})
+  // 主题色
+  const themeSetting = computed(() => {
+    const chartThemeSetting = chartEditStore.getEditCanvasConfig.chartThemeSetting;
+    return chartThemeSetting;
+  });
 
-// 配置项
-const themeColor = computed(() => {
-  const colorCustomMergeData = colorCustomMerge(chartEditStore.getEditCanvasConfig.chartCustomThemeColorInfo)
-  return colorCustomMergeData[chartEditStore.getEditCanvasConfig.chartThemeColor]
-})
+  // 配置项
+  const themeColor = computed(() => {
+    const colorCustomMergeData = colorCustomMerge(
+      chartEditStore.getEditCanvasConfig.chartCustomThemeColorInfo
+    );
+    return colorCustomMergeData[chartEditStore.getEditCanvasConfig.chartThemeColor];
+  });
 
-// 是否展示渲染
-const filterShow = computed(() => {
-  return chartEditStore.getEditCanvasConfig.filterShow
-})
+  // 是否展示渲染
+  const filterShow = computed(() => {
+    return chartEditStore.getEditCanvasConfig.filterShow;
+  });
 
-// 背景
-const rangeStyle = computed(() => {
-  // 设置背景色和图片背景
-  const background = chartEditStore.getEditCanvasConfig.background
-  const backgroundImage = chartEditStore.getEditCanvasConfig.backgroundImage
-  const selectColor = chartEditStore.getEditCanvasConfig.selectColor
-  const backgroundColor = background ? background : undefined
+  // 背景
+  const rangeStyle = computed(() => {
+    // 设置背景色和图片背景
+    const background = chartEditStore.getEditCanvasConfig.background;
+    const backgroundImage = chartEditStore.getEditCanvasConfig.backgroundImage;
+    const selectColor = chartEditStore.getEditCanvasConfig.selectColor;
+    const backgroundColor = background || undefined;
 
-  const computedBackground = selectColor
-    ? { background: backgroundColor }
-    : { background: `url(${backgroundImage}) no-repeat center center / cover !important` }
+    const computedBackground = selectColor
+      ? { background: backgroundColor }
+      : { background: `url(${backgroundImage}) no-repeat center center / cover !important` };
 
-  // @ts-ignore
-  return {
-    ...computedBackground,
-    width: 'inherit',
-    height: 'inherit'
-  }
-})
+    return {
+      ...computedBackground,
+      width: 'inherit',
+      height: 'inherit',
+    };
+  });
 
-onMounted(() => {
-  // 键盘事件
-  useAddKeyboard()
-  // 获取数据
-  dataSyncFetch()
-  // 定时更新数据
-  // intervalDataSyncUpdate()
-})
+  onMounted(() => {
+    // 键盘事件
+    useAddKeyboard();
+    // 获取数据
+    dataSyncFetch();
+    // 定时更新数据
+    // intervalDataSyncUpdate()
+  });
 </script>
 
 <style lang="scss" scoped>
-@include goId('chart-edit-layout') {
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  @extend .go-point-bg;
-  @include background-image('background-point');
-
-  @include goId('chart-edit-content') {
-    border-radius: 10px;
+  @include goId('chart-edit-layout') {
+    position: relative;
+    width: 100%;
     overflow: hidden;
-    @extend .go-transition;
-    @include fetch-theme('box-shadow');
+    @extend .go-point-bg;
+    @include background-image('background-point');
 
-    .edit-content-chart {
-      position: absolute;
+    @include goId('chart-edit-content') {
+      border-radius: 10px;
       overflow: hidden;
+      @extend .go-transition;
+      @include fetch-theme('box-shadow');
+
+      .edit-content-chart {
+        position: absolute;
+        overflow: hidden;
+      }
     }
   }
-}
 </style>
