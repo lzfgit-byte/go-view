985eead8b9237e5ca5c8eb7dc6eee758ceb8ae94
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-21 10:18:43 +0800

    fix:reformat

--- src/const/HttpConst.ts
+++ src/const/HttpConst.ts
@@ -1,4 +1,4 @@
 // 表格数据请求动态参数 TODO 添加更多动态参数
-import { fetchRouteParamsLocation } from '@/utils';
+import { fetchRouteParamsLocation } from '@/utils/router';
 
 export const CustomListReqData = [{ reg: /\$projectId/g, value: fetchRouteParamsLocation() }];
