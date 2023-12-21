759dd599526f8f40b270a9df1110c8e5f000b082
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-21 17:33:42 +0800

    fix:reformat

--- src/packages/components/Charts/index.ts
+++ src/packages/components/Charts/index.ts
@@ -1,8 +1,8 @@
-import Bars from './Bars'
-import Pies from './Pies'
-import Lines from './Lines'
-import Scatters from './Scatters'
-import Mores from './Mores'
-import Maps from './Maps'
+import Bars from './Bars';
+import Pies from './Pies';
+import Lines from './Lines';
+import Scatters from './Scatters';
+import Mores from './Mores';
+import Maps from './Maps';
 
-export const ChartList = [...Bars, ...Lines, ...Pies, ...Scatters, ...Maps, ...Mores]
+export const ChartList = [...Bars, ...Lines, ...Pies, ...Scatters, ...Maps, ...Mores];
