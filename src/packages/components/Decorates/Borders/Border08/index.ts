1e801be678b6ca4fb49c4316b3a55efac9dd6532
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-22 08:43:39 +0800

    fix:reformat

--- src/packages/components/Decorates/Borders/Border08/index.ts
+++ src/packages/components/Decorates/Borders/Border08/index.ts
@@ -1,5 +1,5 @@
-import { ConfigType, PackagesCategoryEnum, ChartFrameEnum } from '@/packages/index.d'
-import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d'
+import { ConfigType, PackagesCategoryEnum, ChartFrameEnum } from '@/packages/index.d';
+import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d';
 
 export const Border08Config: ConfigType = {
   key: 'Border08',
@@ -10,5 +10,5 @@ export const Border08Config: ConfigType = {
   categoryName: ChatCategoryEnumName.BORDER,
   package: PackagesCategoryEnum.DECORATES,
   chartFrame: ChartFrameEnum.STATIC,
-  image: 'border08.png'
-}
+  image: 'border08.png',
+};
--- src/packages/components/Decorates/Borders/Border08/index.vue
+++ src/packages/components/Decorates/Borders/Border08/index.vue
@@ -10,29 +10,16 @@
 
         <mask :id="mask">
           <circle cx="0" cy="0" r="150" :fill="`url(#${gradient})`">
-            <animateMotion
-              :dur="`${dur}s`"
-              :path="pathD"
-              rotate="auto"
-              repeatCount="indefinite"
-            />
+            <animateMotion :dur="`${dur}s`" :path="pathD" rotate="auto" repeatCount="indefinite" />
           </circle>
         </mask>
       </defs>
 
-      <polygon
-        :fill="backgroundColor"
-        :points="`5, 5 ${w - 5}, 5 ${w - 5} ${h - 5} 5, ${h - 5}`"
-      />
+      <polygon :fill="backgroundColor" :points="`5, 5 ${w - 5}, 5 ${w - 5} ${h - 5} 5, ${h - 5}`" />
 
       <use :stroke="colors[0]" stroke-width="1" :xlink:href="`#${path}`" />
 
-      <use
-        :stroke="colors[1]"
-        stroke-width="3"
-        :xlink:href="`#${path}`"
-        :mask="`url(#${mask})`"
-      >
+      <use :stroke="colors[1]" stroke-width="3" :xlink:href="`#${path}`" :mask="`url(#${mask})`">
         <animate
           attributeName="stroke-dasharray"
           :from="`0, ${length}`"
@@ -46,42 +33,40 @@
 </template>
 
 <script setup lang="ts">
-import { PropType, toRefs, computed } from 'vue'
-import { CreateComponentType } from '@/packages/index.d'
-import { getUUID } from '@/utils'
+  import { PropType, toRefs, computed } from 'vue';
+  import { CreateComponentType } from '@/packages/index.d';
+  import { getUUID } from '@/utils';
 
-const props = defineProps({
-  chartConfig: {
-    type: Object as PropType<CreateComponentType>,
-    required: true
-  }
-})
+  const props = defineProps({
+    chartConfig: {
+      type: Object as PropType<CreateComponentType>,
+      required: true,
+    },
+  });
 
-const path = `border-box-08-path-${getUUID()}`
-const gradient = `border-box-08-gradient-${getUUID()}`
-const mask = `border-box-08-mask-${getUUID()}`
+  const path = `border-box-08-path-${getUUID()}`;
+  const gradient = `border-box-08-gradient-${getUUID()}`;
+  const mask = `border-box-08-mask-${getUUID()}`;
 
-const { w, h } = toRefs(props.chartConfig.attr)
-const { colors, dur, backgroundColor, reverse } = toRefs(
-  props.chartConfig.option
-)
+  const { w, h } = toRefs(props.chartConfig.attr);
+  const { colors, dur, backgroundColor, reverse } = toRefs(props.chartConfig.option);
 
-const length = computed(() => {
-  return (w.value + h.value - 5) * 2
-})
+  const length = computed(() => {
+    return (w.value + h.value - 5) * 2;
+  });
 
-const pathD = computed(() => {
-  if (reverse.value)
-    return `M 2.5, 2.5 L 2.5, ${h.value - 2.5} L ${w.value - 2.5}, ${
+  const pathD = computed(() => {
+    if (reverse.value)
+      return `M 2.5, 2.5 L 2.5, ${h.value - 2.5} L ${w.value - 2.5}, ${h.value - 2.5} L ${
+        w.value - 2.5
+      }, 2.5 L 2.5, 2.5`;
+    return `M2.5, 2.5 L${w.value - 2.5}, 2.5 L${w.value - 2.5}, ${h.value - 2.5} L2.5, ${
       h.value - 2.5
-    } L ${w.value - 2.5}, 2.5 L 2.5, 2.5`
-  return `M2.5, 2.5 L${w.value - 2.5}, 2.5 L${w.value - 2.5}, ${
-    h.value - 2.5
-  } L2.5, ${h.value - 2.5} L2.5, 2.5`
-})
+    } L2.5, 2.5`;
+  });
 </script>
 
 <style lang="scss" scoped>
-@include go('border-box') {
-}
+  @include go('border-box') {
+  }
 </style>
