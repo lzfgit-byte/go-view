c121e6a0d6cb082fa3779e7059fce12bccfe8d7f
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-22 09:50:28 +0800

    fix:reformat

--- src/packages/components/Decorates/Borders/Border09/index.ts
+++ src/packages/components/Decorates/Borders/Border09/index.ts
@@ -1,5 +1,5 @@
-import { ConfigType, PackagesCategoryEnum, ChartFrameEnum } from '@/packages/index.d'
-import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d'
+import { ConfigType, PackagesCategoryEnum, ChartFrameEnum } from '@/packages/index.d';
+import { ChatCategoryEnum, ChatCategoryEnumName } from '../../index.d';
 
 export const Border09Config: ConfigType = {
   key: 'Border09',
@@ -10,5 +10,5 @@ export const Border09Config: ConfigType = {
   categoryName: ChatCategoryEnumName.BORDER,
   package: PackagesCategoryEnum.DECORATES,
   chartFrame: ChartFrameEnum.STATIC,
-  image: 'border09.png'
-}
+  image: 'border09.png',
+};
