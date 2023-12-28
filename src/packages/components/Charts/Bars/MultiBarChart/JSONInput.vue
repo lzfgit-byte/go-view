2c0c7c7f9aeb2f7918f6094c80f875fc8cf77010
liuzhongfeng <liuzhongfeng@powerpms.com>
2023-12-28 15:16:00 +0800

    feat:动态柱状图

--- src/packages/components/Charts/Bars/MultiBarChart/JSONInput.vue
+++ src/packages/components/Charts/Bars/MultiBarChart/JSONInput.vue
@@ -10,10 +10,19 @@
       style="width: 70vw"
       title="模态框"
       :bordered="false"
-      size="huge"
+      size="small"
       role="dialog"
       aria-modal="true"
     >
+      <monaco-editor
+        v-model:modelValue="dialogValue"
+        language="json"
+        height="60vh"
+        :editorOptions="{
+          lineNumbers: 'on',
+          minimap: { enabled: true },
+        }"
+      />
       <template #header>
         <n-space justify="space-between">
           <n-h3 class="go-mb-0">编辑json</n-h3>
@@ -25,7 +34,7 @@
       <template #footer>
         <n-space justify="end">
           <n-button @click="showModal = false">取消</n-button>
-          <n-button type="primary" @click="showModal = false">确定</n-button>
+          <n-button type="primary" @click="handlerConfirm">确定</n-button>
         </n-space>
       </template>
     </n-card>
@@ -37,21 +46,30 @@
   import { JSONStringify } from '@/utils';
   import { ref } from 'vue-demi';
   import { icon } from '@/plugins';
+  import { MonacoEditor } from '@/components/Pages/MonacoEditor';
   const props = defineProps({ value: String });
   const emits = defineEmits(['update:value']);
   const valueModel = useVModel(props, 'value', emits);
   const inModelValue = ref(valueModel.value);
-  watch(inModelValue, () => {
+  const setValueModel = (data: string) => {
     let func;
     try {
-      func = new Function(`return ${inModelValue.value}`);
+      func = new Function(`return ${data}`);
     } catch (e) {
       return;
     }
     valueModel.value = JSONStringify(func());
+  };
+  watch(inModelValue, (value) => {
+    setValueModel(value as string);
   });
   const showModal = ref(false);
+  const dialogValue = ref(props.value);
   const { CloseIcon } = icon.ionicons5;
+  const handlerConfirm = () => {
+    setValueModel(dialogValue.value as string);
+    showModal.value = false;
+  };
 </script>
 
 <style scoped lang="scss"></style>
