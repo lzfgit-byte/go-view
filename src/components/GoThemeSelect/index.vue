b9b04206afe08710698987fc8dc98a863defbe44
liuzhongfeng <liuzhongfeng@powerpms.com>
2024-01-04 17:46:18 +0800

    feat:半环图

--- src/components/GoThemeSelect/index.vue
+++ src/components/GoThemeSelect/index.vue
@@ -8,15 +8,15 @@
 </template>
 
 <script lang="ts" setup>
-import { useDesignStore } from '@/store/modules/designStore/designStore'
-import { setHtmlTheme } from '@/utils'
-import { icon } from '@/plugins'
-const { MoonIcon, SunnyIcon } = icon.ionicons5
-const designStore = useDesignStore()
+  import { useDesignStore } from '@/store/modules/designStore/designStore';
+  import { setHtmlTheme } from '@/utils';
+  import { icon } from '@/plugins';
+  const { MoonIcon, SunnyIcon } = icon.ionicons5;
+  const designStore = useDesignStore();
 
-// 改变样式
-const changeTheme = () => {
-  designStore.changeTheme()
-  setHtmlTheme()
-}
+  // 改变样式
+  const changeTheme = () => {
+    designStore.changeTheme();
+    setHtmlTheme();
+  };
 </script>
